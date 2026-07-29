0666c6af4873e2ce3d958dc7b6c90445cc2983af
yartongzimik <zimiklewis@gmail.com>
2026-07-29 21:56:30 +0530

    Align homepage composition with marketplace reference

--- yartong/app/page.tsx
+++ yartong/app/page.tsx
@@ -1,22 +1,18 @@
 import { AdvertisingRail } from "@/components/home/advertising-rail";
-import { DiscoveryRecommendations } from "@/components/home/discovery-recommendations";
 import { Hero } from "@/components/home/hero";
-import { IntentPathways } from "@/components/home/intent-pathways";
-import { TrustAndGrowth } from "@/components/home/trust-and-growth";
+import { MarketplaceShowcase } from "@/components/home/marketplace-showcase";
 import { PublicShell } from "@/components/layout/public-shell";
 
 export default function Home() {
   return (
     <PublicShell>
-      <div className="mx-auto grid w-full max-w-[1540px] gap-5 xl:grid-cols-[minmax(0,1fr)_330px] xl:px-5">
+      <div className="mx-auto grid w-full max-w-[1540px] gap-4 xl:grid-cols-[minmax(0,1fr)_330px] xl:px-4">
         <div className="min-w-0">
           <Hero />
-          <DiscoveryRecommendations />
-          <IntentPathways />
-          <TrustAndGrowth />
+          <MarketplaceShowcase />
         </div>
-        <div className="pt-6 xl:pt-10">
-          <div className="sticky top-5">
+        <div className="px-4 pb-8 pt-4 xl:px-0 xl:pt-5">
+          <div className="sticky top-40">
             <AdvertisingRail />
           </div>
         </div>
