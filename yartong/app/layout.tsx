4b780b454a7f8d0c10cd1829fcba79f24447293f
yartongzimik <zimiklewis@gmail.com>
2026-07-30 00:04:52 +0530

    Install smart interaction feedback globally

--- yartong/app/layout.tsx
+++ yartong/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+
+import { SmartInteractionFeedback } from "@/components/smart-interaction-feedback";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <SmartInteractionFeedback />
+      </body>
     </html>
   );
 }
