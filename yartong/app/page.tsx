6391d1ae2771d1312d053cd597fe6dafc54e7cb9
yartongzimik <zimiklewis@gmail.com>
2026-07-29 21:34:19 +0530

    Restructure homepage with marketplace ad rail

--- yartong/app/page.tsx
+++ yartong/app/page.tsx
@@ -1,3 +1,4 @@
+import { AdvertisingRail } from "@/components/home/advertising-rail";
 import { DiscoveryRecommendations } from "@/components/home/discovery-recommendations";
 import { Hero } from "@/components/home/hero";
 import { IntentPathways } from "@/components/home/intent-pathways";
@@ -7,10 +8,19 @@ import { PublicShell } from "@/components/layout/public-shell";
 export default function Home() {
   return (
     <PublicShell>
-      <Hero />
-      <DiscoveryRecommendations />
-      <IntentPathways />
-      <TrustAndGrowth />
+      <div className="mx-auto grid w-full max-w-[1540px] gap-5 xl:grid-cols-[minmax(0,1fr)_330px] xl:px-5">
+        <div className="min-w-0">
+          <Hero />
+          <DiscoveryRecommendations />
+          <IntentPathways />
+          <TrustAndGrowth />
+        </div>
+        <div className="pt-6 xl:pt-10">
+          <div className="sticky top-5">
+            <AdvertisingRail />
+          </div>
+        </div>
+      </div>
     </PublicShell>
   );
 }
