a88024590761b38527132039faec0d771573f69c
yartongzimik <zimiklewis@gmail.com>
2026-07-30 20:40:12 +0530

    Use full-width light marketplace homepage

--- yartong/app/page.tsx
+++ yartong/app/page.tsx
@@ -1,24 +1,7 @@
-import { AdvertisingRail } from "@/components/home/advertising-rail";
 import { Hero } from "@/components/home/hero";
-import { MarketplaceShowcase } from "@/components/home/marketplace-showcase";
+import { MarketplaceCarousel } from "@/components/home/marketplace-carousel";
 import { PublicShell } from "@/components/layout/public-shell";
 
 export default function Home() {
-  return (
-    <PublicShell>
-      <div className="bg-[#f7f8fa]">
-        <div className="mx-auto grid w-full max-w-[1600px] xl:grid-cols-[minmax(0,1fr)_390px]">
-          <div className="min-w-0 border-r border-slate-200">
-            <Hero />
-            <MarketplaceShowcase />
-          </div>
-          <div className="px-3 pb-8 pt-0">
-            <div className="sticky top-36">
-              <AdvertisingRail />
-            </div>
-          </div>
-        </div>
-      </div>
-    </PublicShell>
-  );
+  return <PublicShell><main className="bg-[#f7f8fa]"><div className="mx-auto w-full max-w-[1600px]"><Hero /><MarketplaceCarousel /></div></main></PublicShell>;
 }
