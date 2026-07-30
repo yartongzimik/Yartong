988a730c3a308f675b747385526dc733d9d636f2
yartongzimik <zimiklewis@gmail.com>
2026-07-30 19:35:43 +0530

    Assemble screenshot-matched Yartong homepage

--- yartong/app/page.tsx
+++ yartong/app/page.tsx
@@ -6,14 +6,16 @@ import { PublicShell } from "@/components/layout/public-shell";
 export default function Home() {
   return (
     <PublicShell>
-      <div className="mx-auto grid w-full max-w-[1540px] gap-4 xl:grid-cols-[minmax(0,1fr)_330px] xl:px-4">
-        <div className="min-w-0">
-          <Hero />
-          <MarketplaceShowcase />
-        </div>
-        <div className="px-4 pb-8 pt-4 xl:px-0 xl:pt-5">
-          <div className="sticky top-40">
-            <AdvertisingRail />
+      <div className="bg-[#f7f8fa]">
+        <div className="mx-auto grid w-full max-w-[1600px] xl:grid-cols-[minmax(0,1fr)_390px]">
+          <div className="min-w-0 border-r border-slate-200">
+            <Hero />
+            <MarketplaceShowcase />
+          </div>
+          <div className="px-3 pb-8 pt-0">
+            <div className="sticky top-36">
+              <AdvertisingRail />
+            </div>
           </div>
         </div>
       </div>
